ebe5fbdfb1e889669647dc297fd461c4f26858c5
Gisele <giseledasilvac@gmail.com>
2024-03-30 22:36:07 -0300

    feat: ajuste swagger

--- src/main/java/com/example/aulaspringweb/controller/UsuarioController.java
+++ src/main/java/com/example/aulaspringweb/controller/UsuarioController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("users/")
+@RequestMapping("/users")
 public class UsuarioController {
 
     @Autowired
